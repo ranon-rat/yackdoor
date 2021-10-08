44e373def79024c66beae9dbb10157061bd69ad5
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-10-07 19:20:02 -0500

    some basic stuff for made this work
    the frontend its really ugly , and later i will change the client for serve to multiple clients for send an individual output to every client
    
    and i want to use fiber cuz its really fast compared with echo but im a little bit lazy

--- static/types/sendCommands.ts
+++ static/types/sendCommands.ts
@@ -1,7 +1,29 @@
-const input:HTMLInputElement=document.getElementById("input") as HTMLInputElement;
-input?.addEventListener("keyup",(e)=>{
-    if (e.key=="Enter"){
-        console.log(input?.value);
+const input: HTMLInputElement = document.getElementById("input") as HTMLInputElement;
+const output=document.getElementById("output") ;
+output!.innerText=""
+input?.addEventListener("keyup", async (e) => {
+    
+    if (e.key == "Enter") {
+
+        const response = await fetch("", {
+            method: "POST",
+            body: input?.value
+        }).catch(e=>null)
+        output!.innerText=input.value+"\n"
+        input!.value = "";
+        
+        const reader = response!.body!.getReader();
+        var gayInterval = setInterval(async function () {
+            const { value, done } = await reader.read();
+            if (done) {clearInterval(gayInterval)
+            };
+            const string=new TextDecoder().decode(value)
+            console.log(string)
+            
+           output!.innerText+=JSON.parse(string)["output"]+"\n"
+          if (output!.innerText.split("\n")!.length>20){
+           output!.innerText=output!.innerText.split("\n").slice(-20).join("\n")}
+        }, 100)
 
     }
 })
